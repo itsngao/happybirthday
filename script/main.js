8e488e4c0e4648bfb32e63168973d27cce448a6b
_itsngao <qn1978234.56@gmail.com>
2026-05-14 19:30:28 +0700

    update homepage

--- script/main.js
+++ script/main.js
@@ -41,7 +41,7 @@ const initializeClock = (nextBirthday) => {
     const countdownElement = document.createElement('div');
     countdownElement.className = 'countdown';
     countdownElement.innerHTML = `
-        <img src="./img/maianhbg.jpg" id="gif"></img>
+        <img src="./img/maianhhome.jpg" id="gif"></img>
         <h1>m.anh's Birthday Countdown</h1>
         <div class="countdown-timer">
             <div class="countdown-item">
